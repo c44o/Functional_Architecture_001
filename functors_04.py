4653f1ab8f910df7bd549feefd765cddbb4526f0
c44o <149115105+c44o@users.noreply.github.com>
2026-05-04 23:14:12 +0300

    Update functors_04.py

--- functors_04.py
+++ functors_04.py
@@ -1,4 +1,14 @@
-
 from pymonad.tools import curry
 from pymonad.maybe import Just, Nothing
 from pymonad.list import ListMonad
+
+@curry(2)
+def add(x, y):
+    return x + y
+
+def add10(functor):
+    return functor.map(add(10))
+
+print(add10(Just(5)))
+print(add10(Nothing))
+print(add10(ListMonad(1, 2, 3, 4)))
