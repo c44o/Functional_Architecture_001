8440e695cdbfbfa8a2e256c583875bec49c15730
c44o <149115105+c44o@users.noreply.github.com>
2026-05-07 23:21:57 +0300

    Update monads_05.py

--- monads_05.py
+++ monads_05.py
@@ -1,1 +1,73 @@
+from pymonad.maybe import Just, Nothing
 
+
+MAX_DIFFERENCE = 4
+
+
+def begin():
+    return Just((0, 0))
+
+
+def is_balanced(left: int, right: int) -> bool:
+    return abs(left - right) <= MAX_DIFFERENCE
+
+
+def to_left(num: int):
+    def step(pole):
+        left, right = pole
+        new_left = left + num
+
+        if is_balanced(new_left, right):
+            return Just((new_left, right))
+
+        return Nothing
+
+    return step
+
+
+def to_right(num: int):
+    def step(pole):
+        left, right = pole
+        new_right = right + num
+
+        if is_balanced(left, new_right):
+            return Just((left, new_right))
+
+        return Nothing
+
+    return step
+
+
+def banana(_pole):
+    return Nothing
+
+
+def show(result):
+    if result == Nothing:
+        print("он упал")
+        return
+
+    print(f"не упал {result.value}")
+
+# упал
+show(
+    begin()
+    .bind(to_left(2))
+    .bind(to_right(5))
+    .bind(to_left(-2))
+)
+# не упал
+show(
+    begin()
+    .bind(to_left(2))
+    .bind(to_right(5))
+    .bind(to_left(-1))
+)
+# упал
+show(
+    begin()
+    .bind(to_left(2))
+    .bind(banana)
+    .bind(to_right(5))
+    .bind(to_left(-1))
+)
